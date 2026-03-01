b3c0d273e2c43cba4ee17bde6191aa5fe0f04735
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-01 14:29:34 -0800

    install

---
+++ instal_package.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+USER=$(id -u)
+LOG_FOLDER="/var/log/install_packages"
+LOG_FILE="/var/log/install_packages/$0.log"
+
+if [$userid -ne 0]; then
+     echo " please run this script using root user" | tee -a $LOG_FILE
+     exit 1
+if
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+    if [$1 -ne 0]; then
+        echo "$2... FAILURE" | tee -a $LOG_FILE
+    else
+        echo "$2... SUCCESS" | tee -a $LOG_FILE
+}
+
+for package in @
+do
+  dns install $package -y &>> LOG_FILE
+  VALIDATE $? "$package installation"
+  done
