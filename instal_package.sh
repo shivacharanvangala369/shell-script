152e56189924e0e2361b8376890564c0216c4c3b
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-01 19:09:21 -0800

    install

--- instal_package.sh
+++ instal_package.sh
@@ -3,9 +3,15 @@
 USERID=$(id -u)
 LOG_FOLDER="/var/log/install_packages"
 LOG_FILE="/var/log/install_packages/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+
 
 if [ $USERID -ne 0 ]; then
-     echo " please run this script using root user" | tee -a $LOG_FILE
+     echo -e "$R please run this script using root user" | tee -a $LOG_FILE
      exit 1
 fi
 
@@ -13,9 +19,9 @@ mkdir -p $LOG_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2... FAILURE" | tee -a $LOG_FILE
+        echo -e " $2... $R FAILURE" | tee -a $LOG_FILE
     else
-        echo "$2... SUCCESS" | tee -a $LOG_FILE
+        echo -e "$2... $G SUCCESS" | tee -a $LOG_FILE
     fi
 }
 
@@ -23,10 +29,10 @@ for package in $@
 do  
   dnf list installed $package  &>>$LOG_FILE
   if [ $? -ne 0 ];then
-    echo "$package is not installed on system, Installing Now"
+    echo -e "$package  $Y is not installed on system, Installing Now"
     dnf install $package -y &>>$LOG_FILE
-    VALIDATE $? "$package installation"
+    VALIDATE  $? "$package  installation"
   else
-    echo "$package is alraedy installed on system"
+    echo -e " $package $Y is alraedy installed on system $N "
   fi
 done
