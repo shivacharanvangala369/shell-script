ce3189d21a9ad03c37b5c5eed9828d2de8fb268e
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-01 19:21:36 -0800

    trap

---
+++ trap.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -echo
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
+USERID=$(id -u)
+LOG_FOLDER="/var/log/install_packages"
+LOG_FILE="/var/log/install_packages/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+
+
+if [ $USERID -ne 0 ]; then
+     echo -e "$R please run this script using root user" | tee -a $LOG_FILE
+     exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+
+
+for package in $@
+do  
+  dnf list installed $package  &>>$LOG_FILE
+  if [ $? -ne 0 ];then
+    echo -e "$package  $Y is not installed on system, Installing Now"
+    dnf install $package -y &>>$LOG_FILE
+      $? "$package  installation"
+  else
+    echo -e " $package $Y is alraedy installed on system $N "
+  fi
+done
