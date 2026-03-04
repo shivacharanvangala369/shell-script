8c3669e794ce6f9ded1872d4b3a1cb70016596d3
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-04 09:05:26 -0800

    removeold logs

---
+++ delete-log-logs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+LOG_DIR=/home/ec2-user/app-logs
+LOG_FILE="$LOG_DIR/$0.log"
+
+if [  ! -d $LOG_DIR ]; then
+    echo -e "$LOG_DIR is not exitst"
+    exit 1
+fi
+
+FILE_TO_DELETE=$(find $LOG_DIR -name "*.log" -mtime +14)
+#echo "$FILE_TO_DELETE"
+
+while IFS=read -r filepath; do
+    echo -e "deleteing files: $filepath"
+    echo -e "deleted files rm -r $filepath"
+done >>>$FILE_TO_DELETE
